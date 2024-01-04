29915d99adf050ee6cc193d4ce104a8c6250cc95
DevAnsar <ansarmirzayi@gmail.com>
2024-01-04 14:24:09 +0330

    Create demo project

---
+++ demo/src/components/Heade.tsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <div className='w-full h-14 border-b flex justify-between items-center px-9'>
+      <div className='flex items-center gap-3'>
+        <h1 className='text-base text-slate-800 font-semibold'>react-search-hook 🔎</h1>
+        <span className='text-sm text-slate-400 font-medium'>v0.2.0</span>
+      </div>
+      <a href='https://github.com/DevAnsar/react-search-hook' className='text-base text-slate-800 hover:text-blue-600'>
+        GitHub
+      </a>
+    </div>
+  )
+}
+
+export default Header
--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -43,7 +43,11 @@ const useSearch = <TItem extends any>(
 
   // Check if the provided store is allowed
   if (context.stores && context.stores[storeName] === undefined) {
-    throw new Error(`Invalid store name: ${storeName}`)
+    throw new Error(`
+    Invalid store name: ${storeName}
+    Make sure you have defined the ${storeName} store for the SearchProvider
+    For more information, please refer to: https://github.com/DevAnsar/react-search-hook?tab=readme-ov-file#2-add-provider-to-top-of-your-component-tree
+  `)
   }
 
   /**
