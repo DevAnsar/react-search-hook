34d112ec0eb0aef849f49905524017f129b062f4
DevAnsar <ansarmirzayi@gmail.com>
2024-01-04 15:55:28 +0330

    update workflow

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-search-hook/',
+  base: '/react-search-hook/docs',
 })
